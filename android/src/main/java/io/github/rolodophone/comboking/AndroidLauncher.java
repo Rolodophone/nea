a220050680b4181fc5434f036ca1b261971f9394
Sergi Lange-Soler <sergi.lange123@gmail.com>
2021-06-09 19:37:00 +0100

    Initial commit

---
+++ android/src/main/java/io/github/rolodophone/comboking/AndroidLauncher.java
@@ -0,0 +1,17 @@
+package io.github.rolodophone.comboking;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import io.github.rolodophone.comboking.Main;
+
+/** Launches the Android application. */
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration configuration = new AndroidApplicationConfiguration();
+		initialize(new Main(), configuration);
+	}
+}
