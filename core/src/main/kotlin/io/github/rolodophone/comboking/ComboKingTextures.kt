1702bd64c8788e38c5f134d09861b047e74687dc
Sergi Lange-Soler <sergi.lange123@gmail.com>
2021-09-29 17:06:41 +0100

    create prototype sprites

--- core/src/main/kotlin/io/github/rolodophone/comboking/ComboKingTextures.kt
+++ core/src/main/kotlin/io/github/rolodophone/comboking/ComboKingTextures.kt
@@ -6,23 +6,11 @@ import com.badlogic.gdx.utils.Disposable
 
 @Suppress("unused","PropertyName")
 class ComboKingTextures: Disposable {
-	private val graphicsAtlas = TextureAtlas(Gdx.files.internal("graphics/graphics.atlas"))
+	private val graphicsAtlas = TextureAtlas(Gdx.files.internal("graphics/sprites.atlas"))
 
-//	val ball = graphicsAtlas.findRegion("ball")!!
-//	val brick_black = graphicsAtlas.findRegion("brick_black")!!
-//	val brick_dark_blue = graphicsAtlas.findRegion("brick_dark_blue")!!
-//	val brick_dark_green = graphicsAtlas.findRegion("brick_dark_green")!!
-//	val brick_light_blue = graphicsAtlas.findRegion("brick_light_blue")!!
-//	val brick_light_green = graphicsAtlas.findRegion("brick_light_green")!!
-//	val brick_orange = graphicsAtlas.findRegion("brick_orange")!!
-//	val brick_pink = graphicsAtlas.findRegion("brick_pink")!!
-//	val brick_purple = graphicsAtlas.findRegion("brick_purple")!!
-//	val brick_red = graphicsAtlas.findRegion("brick_red")!!
-//	val brick_white = graphicsAtlas.findRegion("brick_white")!!
-//	val brick_yellow = graphicsAtlas.findRegion("brick_yellow")!!
-//	val firing_line = graphicsAtlas.findRegion("firing_line")!!
-//	val paddle_normal = graphicsAtlas.findRegion("paddle_normal")!!
-//	val background = graphicsAtlas.findRegion("background")!!
+	val prototype_player = graphicsAtlas.findRegion("prototype_player")!!
+	val prototype_ground = graphicsAtlas.findRegion("prototype_ground")!!
+	val prototype_platform = graphicsAtlas.findRegion("prototype_platform")!!
 
 	override fun dispose() {
 		graphicsAtlas.dispose()
