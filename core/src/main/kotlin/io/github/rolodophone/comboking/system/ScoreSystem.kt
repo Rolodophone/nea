007107cb5cd611c02633caf008eb76bceac9c266
Sergi Lange-Soler <sergi.lange123@gmail.com>
2021-10-17 19:42:39 +0100

    add stubs for ScoreSystem and SpawningSystem

---
+++ core/src/main/kotlin/io/github/rolodophone/comboking/system/ScoreSystem.kt
@@ -0,0 +1,12 @@
+package io.github.rolodophone.comboking.system
+
+import com.badlogic.ashley.core.EntitySystem
+
+/**
+ * Increases the score when certain game events are triggered.
+ */
+class ScoreSystem(
+) : EntitySystem() {
+	override fun update(deltaTime: Float) {
+	}
+}
---
+++ core/src/main/kotlin/io/github/rolodophone/comboking/system/SpawningSystem.kt
@@ -0,0 +1,12 @@
+package io.github.rolodophone.comboking.system
+
+import com.badlogic.ashley.core.EntitySystem
+
+/**
+ * Spawns entities such as enemies and items semi-randomly.
+ */
+class SpawningSystem(
+) : EntitySystem() {
+	override fun update(deltaTime: Float) {
+	}
+}
