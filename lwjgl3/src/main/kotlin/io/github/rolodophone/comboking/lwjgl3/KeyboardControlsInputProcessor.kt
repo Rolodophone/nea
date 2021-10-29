fe28d3f1a0ee9375d983149126277f451721fa8d
Sergi Lange-Soler <sergi.lange123@gmail.com>
2021-10-29 22:30:15 +0100

    ability to run left and right using keyboard

--- lwjgl3/src/main/kotlin/io/github/rolodophone/comboking/lwjgl3/KeyboardControlsInputProcessor.kt
+++ lwjgl3/src/main/kotlin/io/github/rolodophone/comboking/lwjgl3/KeyboardControlsInputProcessor.kt
@@ -1,7 +1,11 @@
 package io.github.rolodophone.comboking.lwjgl3
 
 
+import com.badlogic.gdx.Gdx
+import com.badlogic.gdx.Input
 import com.badlogic.gdx.InputProcessor
+import io.github.rolodophone.comboking.MoveAction
+import io.github.rolodophone.comboking.event.GameEvent
 import io.github.rolodophone.comboking.event.GameEventManager
 
 /**
@@ -14,10 +18,48 @@ class KeyboardControlsInputProcessor(
 	// to indicate that it should be passed on.
 
 	override fun keyDown(keycode: Int): Boolean {
+		when (keycode) {
+			Input.Keys.A -> {
+				GameEvent.PlayerMoveInput.moveAction = MoveAction.RUN_LEFT
+				gameEventManager.trigger(GameEvent.PlayerMoveInput)
+				return true
+			}
+			Input.Keys.D -> {
+				GameEvent.PlayerMoveInput.moveAction = MoveAction.RUN_RIGHT
+				gameEventManager.trigger(GameEvent.PlayerMoveInput)
+				return true
+			}
+		}
+
 		return false
 	}
 
 	override fun keyUp(keycode: Int): Boolean {
+		when (keycode) {
+			Input.Keys.A -> {
+				if (Gdx.input.isKeyPressed(Input.Keys.D)) {
+					GameEvent.PlayerMoveInput.moveAction = MoveAction.RUN_RIGHT
+					gameEventManager.trigger(GameEvent.PlayerMoveInput)
+				}
+				else {
+					GameEvent.PlayerMoveInput.moveAction = MoveAction.STOP
+					gameEventManager.trigger(GameEvent.PlayerMoveInput)
+				}
+				return true
+			}
+			Input.Keys.D -> {
+				if (Gdx.input.isKeyPressed(Input.Keys.A)) {
+					GameEvent.PlayerMoveInput.moveAction = MoveAction.RUN_LEFT
+					gameEventManager.trigger(GameEvent.PlayerMoveInput)
+				}
+				else {
+					GameEvent.PlayerMoveInput.moveAction = MoveAction.STOP
+					gameEventManager.trigger(GameEvent.PlayerMoveInput)
+				}
+				return true
+			}
+		}
+
 		return false
 	}
 
